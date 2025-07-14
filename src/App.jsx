71c67f318fae2bec50c9d1b1a1279c56cb55e85a
nopalss <holanakdo@gmail.com>
2025-07-14 12:47:39 +0700

    fixed

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import Header from './template/Header'
 import Main from './template/Main'
 import Footer from './template/Footer'
-import './app.css'
+import './App.css'
 
 export const App = () => {
   return (
