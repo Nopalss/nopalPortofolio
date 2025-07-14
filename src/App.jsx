18693808b1c799a27e652bdede10bbcaaf2c8f6a
nopalss <holanakdo@gmail.com>
2025-07-14 12:54:40 +0700

    add transition

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,23 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import Header from './template/Header'
 import Main from './template/Main'
 import Footer from './template/Footer'
 import './App.css'
+import ScrollReveal from "scrollreveal";
 
 export const App = () => {
+  useEffect(() => {
+    const sr = ScrollReveal({
+      distance: "40px",
+      duration: 2050,
+      delay: 200,
+      reset: false,
+    });
+    sr.reveal(".bottom", { origin: "bottom" });
+    sr.reveal(".top", { origin: "top" });
+    sr.reveal(".left", { origin: "left" });
+    sr.reveal(".right", { origin: "right" });
+  });
   return (
     
     <div className='body'>
